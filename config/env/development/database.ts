1f6860c540eacb469be9bf42fd93876b9ad7f61f
Rafif Fathi Misbah <rafiff@thebigrichgroup.com>
2025-02-13 11:10:50 +0700

    Guest & Event table added

--- config/env/development/database.ts
+++ config/env/development/database.ts
@@ -2,8 +2,9 @@ export default ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            connectionString: env('DATABASE_URL')
+            connectionString: env('DATABASE_URL'),
+            ssl: env.bool('DATABASE_SSL', true) ? { rejectUnauthorized: false } : false,
         },
         acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-    }
+    },
 });
